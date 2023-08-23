f6e28ef940caa725de8b932e0769edcab41f0784
dfermic2 <dfermic2@etf.unsa.ba>
2023-08-23 17:10:38 +0200

    Implemented row2object for RezervacijaDaoSQLImplementation

--- src/main/java/ba/unsa/etf/rpr/dao/RezervacijaDaoSQLImplementation.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RezervacijaDaoSQLImplementation.java
@@ -3,7 +3,10 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Rezervacija;
 import ba.unsa.etf.rpr.exceptions.DolinaSreceException;
 
+import java.math.BigDecimal;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
 
 public class RezervacijaDaoSQLImplementation extends AbstractDao<Rezervacija> implements RezervacijaDao {
     public RezervacijaDaoSQLImplementation(String tableName) {
@@ -12,6 +15,19 @@ public class RezervacijaDaoSQLImplementation extends AbstractDao<Rezervacija> im
 
     @Override
     public Rezervacija row2object(ResultSet rs) throws DolinaSreceException {
+        try {
+            int id = rs.getInt("id");
+            int idKorisnik = rs.getInt("id_korisnik");
+            int idKucica = rs.getInt("id_kucica");
+            Date pocetak = rs.getDate("pocetak");
+            Date kraj = rs.getDate("kraj");
+            BigDecimal cijena = rs.getBigDecimal("cijena");
+
+            return new Rezervacija(id, idKorisnik, idKucica, pocetak, kraj, cijena);
+        } catch (SQLException e) {
+            throw new DolinaSreceException(e.getMessage(), e);
+        }
+
         return null;
     }
 }
