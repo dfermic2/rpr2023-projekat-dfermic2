1f567d675423a57ed77175e3d115945c9cfe045a
dfermic2 <dfermic2@etf.unsa.ba>
2023-08-24 16:21:58 +0200

    Implemented row2object for KucicaDaoSQLImplementation

--- src/main/java/ba/unsa/etf/rpr/dao/KucicaDaoSQLImplementation.java
+++ src/main/java/ba/unsa/etf/rpr/dao/KucicaDaoSQLImplementation.java
@@ -3,15 +3,29 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Kucica;
 import ba.unsa.etf.rpr.exceptions.DolinaSreceException;
 
+import java.math.BigDecimal;
+import java.sql.Blob;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class KucicaDaoSQLImplementation extends AbstractDao<Kucica> implements KucicaDao {
 
     public KucicaDaoSQLImplementation() {
         super("kucice");
     }
+
     @Override
     public Kucica row2object(ResultSet rs) throws DolinaSreceException {
-        return null;
+        try {
+            int id = rs.getInt("id");
+            String ime = rs.getString("ime");
+            BigDecimal cijena = rs.getBigDecimal("cijena");
+            boolean jacuzzi = rs.getBoolean("jacuzzi");
+            Blob slika = rs.getBlob("slika");
+
+            return new Kucica(id, ime, cijena, jacuzzi, slika);
+        } catch (SQLException e) {
+            throw new DolinaSreceException(e.getMessage(), e);
+        }
     }
 }
