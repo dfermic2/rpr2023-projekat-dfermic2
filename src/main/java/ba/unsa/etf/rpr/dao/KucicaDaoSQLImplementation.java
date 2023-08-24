79ae8ca3ab1a86831d7eb017d6b804785245079c
dfermic2 <dfermic2@etf.unsa.ba>
2023-08-24 16:19:17 +0200

    Added constructor to KucicaDaoSQLImplementation

--- src/main/java/ba/unsa/etf/rpr/dao/KucicaDaoSQLImplementation.java
+++ src/main/java/ba/unsa/etf/rpr/dao/KucicaDaoSQLImplementation.java
@@ -6,6 +6,10 @@ import ba.unsa.etf.rpr.exceptions.DolinaSreceException;
 import java.sql.ResultSet;
 
 public class KucicaDaoSQLImplementation extends AbstractDao<Kucica> implements KucicaDao {
+
+    public KucicaDaoSQLImplementation() {
+        super("kucice");
+    }
     @Override
     public Kucica row2object(ResultSet rs) throws DolinaSreceException {
         return null;
