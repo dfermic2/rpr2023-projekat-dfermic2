29a5502122be04b79463a4408f0501a0909a40e7
dfermic2 <dfermic2@etf.unsa.ba>
2023-08-25 17:22:33 +0200

    Refactoring findByEmail

--- src/main/java/ba/unsa/etf/rpr/business/KorisnikManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/KorisnikManager.java
@@ -1,13 +1,11 @@
 package ba.unsa.etf.rpr.business;
 
-import ba.unsa.etf.rpr.dao.KorisnikDao;
-import ba.unsa.etf.rpr.dao.KorisnikDaoSQLImplementation;
+import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Korisnik;
 import ba.unsa.etf.rpr.exceptions.DolinaSreceException;
 
 public class KorisnikManager {
     public static Korisnik findByEmail(String email) throws DolinaSreceException {
-        KorisnikDao korisnikDao = new KorisnikDaoSQLImplementation();
-        return korisnikDao.getByEmail(email);
+        return DaoFactory.korisnikDao().getByEmail(email);
     }
 }
