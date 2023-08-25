cd851f36fb8d2997bf506d5262c86d996b8dda74
dfermic2 <dfermic2@etf.unsa.ba>
2023-08-25 19:43:29 +0200

    Implemented scene switchin after login

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -13,8 +13,6 @@ public class AppFX extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
-        LoginController loginController = new LoginController();
-        fxmlLoader.setController(loginController);
         Parent root = fxmlLoader.load();
         stage.setTitle("Dolina sreće");
         stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -4,27 +4,39 @@ import ba.unsa.etf.rpr.business.KorisnikManager;
 import ba.unsa.etf.rpr.domain.Korisnik;
 import ba.unsa.etf.rpr.exceptions.DolinaSreceException;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
 
     @FXML
     public TextField email, password;
 
-     @FXML
-     public Label loginMessage;
+    @FXML
+    public Label loginMessage;
 
-    public void onLogin() throws DolinaSreceException {
+    public void onLogin(javafx.event.ActionEvent actionEvent) throws DolinaSreceException, IOException {
 
-        if(email.getText().isEmpty()) loginMessage.setText("Unesite email!");
-        else if(password.getText().isEmpty()) loginMessage.setText("Unesite password!");
+        if (email.getText().isEmpty()) loginMessage.setText("Unesite email!");
+        else if (password.getText().isEmpty()) loginMessage.setText("Unesite password!");
         else {
             Korisnik korisnik = KorisnikManager.findByEmail(email.getText());
-            if(korisnik == null) loginMessage.setText("Unijeli ste neispravne podatke!");
-            else if (!korisnik.getPassword().equals(password.getText())) loginMessage.setText("Unijeli ste neispravne podatke!");
-            else loginMessage.setText("");
+            if (korisnik == null) loginMessage.setText("Unijeli ste neispravne podatke!");
+            else if (!korisnik.getPassword().equals(password.getText()))
+                loginMessage.setText("Unijeli ste neispravne podatke!");
+            else {
+                Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/kucice.fxml"));
+                Scene scene = new Scene(fxmlLoader.load());
+                stage.setScene(scene);
+                stage.show();
+            }
         }
     }
 }
