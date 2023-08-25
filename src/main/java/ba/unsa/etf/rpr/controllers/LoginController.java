acae0a4cf844919de32bb36ea18cfccf78e18efa
dfermic2 <dfermic2@etf.unsa.ba>
2023-08-25 19:11:35 +0200

    Added loginMessage

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -5,22 +5,26 @@ import ba.unsa.etf.rpr.domain.Korisnik;
 import ba.unsa.etf.rpr.exceptions.DolinaSreceException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class LoginController {
 
     @FXML
     public TextField email, password;
-    public Button loginBtn;
+
+     @FXML
+     public Label loginMessage;
 
     public void onLogin() throws DolinaSreceException {
 
-        if(email.getText().isEmpty()) System.out.println("Prazan email");
-        if(password.getText().isEmpty()) System.out.println("Prazan password");
+        if(email.getText().isEmpty()) loginMessage.setText("Unesite email!");
+        else if(password.getText().isEmpty()) loginMessage.setText("Unesite password!");
         else {
             Korisnik korisnik = KorisnikManager.findByEmail(email.getText());
-            if (korisnik.getPassword().equals(password.getText())) System.out.println("Ispravan password");
-            else System.out.println("Pogresan password");
+            if(korisnik == null) loginMessage.setText("Unijeli ste neispravne podatke!");
+            else if (!korisnik.getPassword().equals(password.getText())) loginMessage.setText("Unijeli ste neispravne podatke!");
+            else loginMessage.setText("");
         }
     }
 }
