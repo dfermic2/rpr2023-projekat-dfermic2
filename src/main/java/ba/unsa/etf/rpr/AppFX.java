bc9b5f2e1c5a1a94a409edf66af479ee0276c824
dfermic2 <dfermic2@etf.unsa.ba>
2023-08-25 17:48:47 +0200

    Addded login controller, created stage and added scene and root in AppFX

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -1,11 +1,25 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.controllers.LoginController;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class AppFX extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        LoginController homeController = new LoginController();
+        fxmlLoader.setController(homeController);
+        Parent root = fxmlLoader.load();
+        stage.setTitle("Dolina sreće");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
     }
 
     public static void main(String[] args) {
