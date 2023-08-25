7b49604da973dafe2b969c1735e22342d95953a5
dfermic2 <dfermic2@etf.unsa.ba>
2023-08-25 14:57:16 +0200

    Adding javaFX to project

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -8,7 +8,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class App {
-    public static void main(String[] args) throws DolinaSreceException, SQLException {
+    public static void main(String[] args) {
 
 //        KorisnikDao korisnikDao = new KorisnikDaoSQLImplementation();
 //        Connection connection = KorisnikDaoSQLImplementation.getConnection();
---
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rpr;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppFX extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        System.out.println("STARTED");
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
