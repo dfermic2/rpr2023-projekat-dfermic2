e6a3d7fe2202d702baf9e274cc84598ae2aedfcf
dfermic2 <dfermic2@etf.unsa.ba>
2023-08-27 15:07:13 +0200

    Added setData to KucicaController

--- src/main/java/ba/unsa/etf/rpr/controllers/KucicaController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/KucicaController.java
@@ -1,4 +1,30 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.domain.Kucica;
+import ba.unsa.etf.rpr.exceptions.DolinaSreceException;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.awt.*;
+import java.io.ByteArrayInputStream;
+
 public class KucicaController {
+
+    @FXML
+    Label ime, jacuzzi, cijena;
+
+    @FXML
+    ImageView slika;
+
+    public void setData(Kucica kucica) {
+        ime.setText(kucica.getIme());
+        cijena.setText(cijena + "KM");
+
+        if (kucica.isJacuzzi()) jacuzzi.setText("Jacuzzi");
+        else jacuzzi.setText("");
+
+        Image image = new Image(new ByteArrayInputStream(kucica.getSlika()));
+        slika.setImage(image);
+    }
 }
