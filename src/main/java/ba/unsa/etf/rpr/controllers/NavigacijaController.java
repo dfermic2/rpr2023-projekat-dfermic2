7db865df827f72989c8aea93711d3ca45ea15220
dfermic2 <dfermic2@etf.unsa.ba>
2023-09-07 19:19:12 +0200

    Added o Nama

--- src/main/java/ba/unsa/etf/rpr/controllers/NavigacijaController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/NavigacijaController.java
@@ -1,4 +1,27 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class NavigacijaController {
+
+    public void onPocetna(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/kucice.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void onONama(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/oNama.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
 }
