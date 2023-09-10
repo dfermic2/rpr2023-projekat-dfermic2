9cb73cba89395e5234b4b4e97b272e105ed3da96
dfermic2 <dfermic2@etf.unsa.ba>
2023-09-10 23:13:07 +0200

    Implemented RezervacijaManager

--- src/main/java/ba/unsa/etf/rpr/business/RezervacijaManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/RezervacijaManager.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.Set;
+
 public class RezervacijaManager {
+    public static Set<Integer> findBetweenDates(LocalDate date) {
+        return DaoFactory.rezervacijaDao().getBetweenDates(Date.from(date.atStartOfDay(ZoneId.systemDefault()).toInstant()));
+    }
 }
