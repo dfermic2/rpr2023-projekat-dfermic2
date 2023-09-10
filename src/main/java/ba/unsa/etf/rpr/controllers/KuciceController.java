870ff07078dbb561c558e767836dfe005b9aba1b
dfermic2 <dfermic2@etf.unsa.ba>
2023-09-10 23:50:43 +0200

    MVP of filtering

--- src/main/java/ba/unsa/etf/rpr/controllers/KuciceController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/KuciceController.java
@@ -19,6 +19,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class KuciceController implements Initializable {
 
@@ -28,20 +29,22 @@ public class KuciceController implements Initializable {
     @FXML
     DatePicker pocetakDate, krajDate;
 
+    List<Kucica> kucicaList;
+
+    {
+        try {
+            kucicaList = KucicaManager.getAll();
+        } catch (DolinaSreceException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
-            List<Kucica> kucicaList = KucicaManager.getAll();
 
-            for (Kucica kucica : kucicaList) {
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/kucica.fxml"));
-                VBox vBox = fxmlLoader.load();
-                KucicaController kucicaController = fxmlLoader.getController();
-                kucicaController.setData(kucica);
-                kuciceLayout.getChildren().add(vBox);
-
-
-                // TODO: REFACTOR
+            showKucice(kucicaList);
+            // TODO: REFACTOR
                 pocetakDate.setDayCellFactory(param -> new DateCell() {
                     @Override
                     public void updateItem(LocalDate date, boolean empty) {
@@ -57,18 +60,34 @@ public class KuciceController implements Initializable {
                         setDisable(empty || date.isBefore(pocetakDate.getValue()));
                     }
                 });
-            }
 
             pocetakDate.setValue(LocalDate.now());
 
-        } catch (IOException | DolinaSreceException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void onTrazi(ActionEvent actionEvent) {
+    public void onTrazi(ActionEvent actionEvent) throws IOException {
         //Uzmi sve kucice kod kojih su ispunjeni uvijeti
         Set<Integer> kuciceId = RezervacijaManager.findBetweenDates(pocetakDate.getValue());
-        System.out.println("KUCICA PRONADJENA: " + kuciceId);
+        kuciceId.addAll(RezervacijaManager.findBetweenDates(krajDate.getValue()));
+
+        List<Kucica> kuciceFiltered = kucicaList.stream().filter(kucica -> !kuciceId.contains(kucica.getId())).collect(Collectors.toList());
+
+        kuciceLayout.getChildren().clear();
+        showKucice(kuciceFiltered);
+
+        System.out.println("KUCICA: " + kuciceFiltered);
+    }
+
+    private void showKucice(List<Kucica> kucicaList) throws IOException {
+        for (Kucica kucica : kucicaList) {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/kucica.fxml"));
+            VBox vBox = fxmlLoader.load();
+            KucicaController kucicaController = fxmlLoader.getController();
+            kucicaController.setData(kucica);
+            kuciceLayout.getChildren().add(vBox);
+        }
     }
 }
