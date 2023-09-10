df1c635905b5147d2cbc24d89ca286df4524e3da
dfermic2 <dfermic2@etf.unsa.ba>
2023-09-11 00:38:59 +0200

    Implemented add rezervacija

--- src/main/java/ba/unsa/etf/rpr/business/RezervacijaManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/RezervacijaManager.java
@@ -1,6 +1,8 @@
 package ba.unsa.etf.rpr.business;
 
 import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Rezervacija;
+import ba.unsa.etf.rpr.exceptions.DolinaSreceException;
 
 import java.util.Date;
 import java.util.Set;
@@ -8,5 +10,9 @@ import java.util.Set;
 public class RezervacijaManager {
     public static Set<Integer> findBetweenDates(Date date) {
         return DaoFactory.rezervacijaDao().getBetweenDates(date);
+    }
+
+    public static Rezervacija add(Rezervacija rezervacija) throws DolinaSreceException {
+        return DaoFactory.rezervacijaDao().add(rezervacija);
     }
 }
