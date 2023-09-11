0c438bbc71b90f55281e362f8811314d4880733d
dfermic2 <dfermic2@etf.unsa.ba>
2023-09-11 19:41:31 +0200

    Added javadoc for RezervacijaDao

--- src/main/java/ba/unsa/etf/rpr/dao/RezervacijaDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RezervacijaDao.java
@@ -3,10 +3,13 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Rezervacija;
 
 import java.time.LocalDate;
-import java.util.Date;
-import java.util.List;
 import java.util.Set;
 
+/**
+ * Dao interface for Rezervacija model
+ *
+ * @author Dijana Fermic
+ */
 public interface RezervacijaDao extends Dao<Rezervacija> {
 
     Set<Integer> getBetweenDates(LocalDate date);
