67041763c6aad4a4ee77c37f00410a14096f095d
dfermic2 <dfermic2@etf.unsa.ba>
2023-09-11 19:40:54 +0200

    Added javadoc for KucicaDao

--- src/main/java/ba/unsa/etf/rpr/dao/KucicaDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/KucicaDao.java
@@ -2,5 +2,10 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Kucica;
 
+/**
+ * Dao interface for Kucica model
+ *
+ * @author Dijana Fermic
+ */
 public interface KucicaDao extends Dao<Kucica> {
 }
