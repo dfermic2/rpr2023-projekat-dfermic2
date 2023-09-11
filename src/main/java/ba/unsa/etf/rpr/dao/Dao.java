1c5fd2d2576da2c2ecdc658514fb0ec33886c02b
dfermic2 <dfermic2@etf.unsa.ba>
2023-09-11 19:35:17 +0200

    Added javadoc for getById

--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -10,7 +10,14 @@ import java.util.List;
  * @author Dijana Fermic
  */
 public interface Dao<T> {
+    /**
+     * get entity from database with given id
+     * @param id primary key of entity
+     * @return Entity from database
+     * @throws DolinaSreceException - user defined exception
+     */
     T getById(int id) throws DolinaSreceException;
+
 
     List<T> getAll() throws DolinaSreceException;
 
