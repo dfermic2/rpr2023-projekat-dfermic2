87300fc3935e25cc33aaf424e627e19178f9485b
dfermic2 <dfermic2@etf.unsa.ba>
2023-09-11 23:30:50 +0200

    Unos datuma

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -4,6 +4,8 @@ import ba.unsa.etf.rpr.business.KorisnikManager;
 import ba.unsa.etf.rpr.domain.Korisnik;
 import ba.unsa.etf.rpr.exceptions.DolinaSreceException;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -46,6 +48,7 @@ public class App {
                 biranjeLoginRegistracija();
             } else {
                 System.out.println("Uspješno ste prijavljeni!");
+                rezervacija();
             }
         } catch (DolinaSreceException e) {
             throw new RuntimeException(e);
@@ -77,11 +80,11 @@ public class App {
         try {
             Korisnik korisnik = KorisnikManager.findByEmail(email);
 
-            if(ime.isEmpty() || prezime.isEmpty() || email.isEmpty() || adresa.isEmpty() || password.isEmpty()) {
+            if (ime.isEmpty() || prezime.isEmpty() || email.isEmpty() || adresa.isEmpty() || password.isEmpty()) {
                 System.out.println("Nepotpuni podatci!");
                 biranjeLoginRegistracija();
             }
-            if(korisnik != null) {
+            if (korisnik != null) {
                 System.out.println("Email adresa vec postoji!");
                 biranjeLoginRegistracija();
             }
@@ -90,9 +93,47 @@ public class App {
                 biranjeLoginRegistracija();
             }
 
+            System.out.println("Uspješno ste registrovani!");
+
         } catch (DolinaSreceException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    //Validacija da je pocetak prije kraja i sl.
+    private static void rezervacija() {
+        System.out.println("Početak rezervacije");
+        LocalDate pocetak = unosDatuma();
+
+        System.out.println("Kraj rezervacije");
+        LocalDate kraj = unosDatuma();
+
+
+    }
+
+    private static LocalDate unosDatuma() {
+        System.out.println("Unesite datum u formatu yyyy-MM-dd: ");
+        Scanner scanner = new Scanner(System.in);
+        String datum = scanner.nextLine();
+        LocalDate localDate = returnValidDate(datum);
+
+        if(localDate == null) {
+            System.out.println("Pogršan unos!");
+            unosDatuma();
+        };
+        return localDate;
+    }
+
+    private static LocalDate returnValidDate(String datum) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate localDate;
+
+        try {
+            localDate = (LocalDate) dateTimeFormatter.parse(datum);
+        } catch (Exception e) {
+            return null;
+        }
+
+        return localDate;
     }
 }
