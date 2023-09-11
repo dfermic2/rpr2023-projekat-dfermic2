3fee7eb973d6bf6b9a24fe24329026ce58433879
dfermic2 <dfermic2@etf.unsa.ba>
2023-09-11 16:17:31 +0200

    Added test for testObject2row

--- src/test/java/ba/unsa/etf/rpr/dao/KorisnikDaoSQLImplementationTest.java
+++ src/test/java/ba/unsa/etf/rpr/dao/KorisnikDaoSQLImplementationTest.java
@@ -3,17 +3,18 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Korisnik;
 import ba.unsa.etf.rpr.exceptions.DolinaSreceException;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Map;
+import java.util.TreeMap;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class KorisnikDaoSQLImplementationTest {
-
-
     @Test
     public void testRow2object() throws SQLException, DolinaSreceException {
 
@@ -33,8 +34,21 @@ public class KorisnikDaoSQLImplementationTest {
         assertEquals(korisnikDaoSQLImplementation.row2object(resultSet), korisnik);
     }
 
+    @Test
     public void testObject2row() {
+        KorisnikDaoSQLImplementation korisnikDaoSQLImplementation = (KorisnikDaoSQLImplementation) DaoFactory.korisnikDao();
+        Korisnik korisnik = new Korisnik("Mujo", "Mujic", "mujo@mujo.com", "Mu Town 99", "mujo123");
 
+        Map<String, Object> row = new TreeMap<>();
+        row.put("id", korisnik.getId());
+        row.put("ime", korisnik.getIme());
+        row.put("prezime", korisnik.getPrezime());
+        row.put("email", korisnik.getEmail());
+        row.put("adresa", korisnik.getAdresa());
+        row.put("password", korisnik.getPassword());
+        korisnik.setId(1);
+
+        assertEquals(korisnikDaoSQLImplementation.object2row(korisnik), row);
 
     }
 
