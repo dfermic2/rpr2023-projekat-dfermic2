3aef94463b6e5599e8162cf4be1d8b7946e71686
dfermic2 <dfermic2@etf.unsa.ba>
2023-09-11 22:40:56 +0200

    biranje login ili registracija

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,7 +1,34 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        biranjeLoginRegistracija();
+    }
 
+    private static void biranjeLoginRegistracija() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Unesite 1 za prijavu, 2 za registraciju: ");
+
+        int opcija = scanner.nextInt();
+
+        if(opcija == 1) {
+            prijava();
+        } else if (opcija == 2) {
+            registracija();
+        } else {
+            System.out.println("Neispravan unos, pokušajte ponovo");
+            biranjeLoginRegistracija();
+        }
+    }
+
+    private static void prijava() {
+        System.out.println("unijeli ste 1");
+    }
+
+    private static void registracija() {
+        System.out.println("Unijeli ste 2");
     }
 }
