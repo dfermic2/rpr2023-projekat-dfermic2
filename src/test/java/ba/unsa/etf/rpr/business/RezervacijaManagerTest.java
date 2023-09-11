b2637b48770c4b5dd81ffba0c0849c659f836fe7
dfermic2 <dfermic2@etf.unsa.ba>
2023-09-11 17:42:54 +0200

    Added test class for rezervacijaManager

---
+++ src/test/java/ba/unsa/etf/rpr/business/RezervacijaManagerTest.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr.business;
+
+import junit.framework.TestCase;
+
+public class RezervacijaManagerTest extends TestCase {
+
+    public void testFindBetweenDates() {
+    }
+
+    public void testAdd() {
+    }
+}
