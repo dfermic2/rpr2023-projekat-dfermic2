3bcaba29c4b0ab27a13b6415c3255782077d8135
dfermic2 <dfermic2@etf.unsa.ba>
2023-09-11 19:40:31 +0200

    Added javadoc for KorisnikDao

--- src/main/java/ba/unsa/etf/rpr/dao/KorisnikDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/KorisnikDao.java
@@ -3,6 +3,11 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Korisnik;
 import ba.unsa.etf.rpr.exceptions.DolinaSreceException;
 
+/**
+ * Dao interface for Korisnik model
+ *
+ * @author Dijana Fermic
+ */
 public interface KorisnikDao extends Dao<Korisnik> {
     Korisnik getByEmail(String email) throws DolinaSreceException;
 }
