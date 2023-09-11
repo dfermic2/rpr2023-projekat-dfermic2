fae9c732d4d6d5ba23370b62a41e702f649a6dae
dfermic2 <dfermic2@etf.unsa.ba>
2023-09-11 21:55:29 +0200

    Added login and register options

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,6 +1,15 @@
 package ba.unsa.etf.rpr;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+
 public class App {
+    private static final Option LOGIN = new Option("l", "login", false, "Logujte se u aplikaciju");
+    private static final Option REGISTER = new Option("r", "register", false, "Registrujte se u aplikaciju");
+
+
+
     public static void main(String[] args) {
+
     }
 }
