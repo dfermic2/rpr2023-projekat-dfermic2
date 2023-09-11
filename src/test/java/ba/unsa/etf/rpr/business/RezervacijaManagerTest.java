ee33cb698d919cd100a9106beeb0a72d0141f9b6
dfermic2 <dfermic2@etf.unsa.ba>
2023-09-11 17:46:04 +0200

    Added test for findBetweenDates

--- src/test/java/ba/unsa/etf/rpr/business/RezervacijaManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/business/RezervacijaManagerTest.java
@@ -1,10 +1,28 @@
 package ba.unsa.etf.rpr.business;
 
-import junit.framework.TestCase;
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.dao.RezervacijaDao;
+import org.junit.Test;
+import org.mockito.MockedStatic;
 
-public class RezervacijaManagerTest extends TestCase {
+import java.time.LocalDate;
 
+import static org.mockito.Mockito.*;
+
+public class RezervacijaManagerTest {
+
+    @Test
     public void testFindBetweenDates() {
+        LocalDate date = LocalDate.now();
+        RezervacijaDao rezervacijaDao = mock(RezervacijaDao.class);
+        MockedStatic<DaoFactory> daoFactoryMockedStatic = mockStatic(DaoFactory.class);
+        daoFactoryMockedStatic.when(DaoFactory::rezervacijaDao).thenReturn(rezervacijaDao);
+
+        RezervacijaManager.findBetweenDates(date);
+
+        verify(rezervacijaDao).getBetweenDates(date);
+
+        daoFactoryMockedStatic.close();
     }
 
     public void testAdd() {
