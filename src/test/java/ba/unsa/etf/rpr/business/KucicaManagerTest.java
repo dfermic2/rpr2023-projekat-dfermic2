fa3fddce1e243b9cf26c55fd2d8616495db11f79
dfermic2 <dfermic2@etf.unsa.ba>
2023-09-11 17:38:19 +0200

    Added test class for kucicaManager

---
+++ src/test/java/ba/unsa/etf/rpr/business/KucicaManagerTest.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rpr.business;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class KucicaManagerTest extends TestCase {
+
+    @Test
+    public void testFindById() {
+    }
+
+    public void testGetAll() {
+    }
+}
