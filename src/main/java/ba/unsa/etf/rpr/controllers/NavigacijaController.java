92bfc2f4ba8f0441f86f810397a88a6feb42ac3e
dfermic2 <dfermic2@etf.unsa.ba>
2023-09-12 02:34:44 +0200

    Change cursor for navigation

--- src/main/java/ba/unsa/etf/rpr/controllers/NavigacijaController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/NavigacijaController.java
@@ -1,19 +1,36 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * JavaFX controller for navigating the application
  *
  * @author Dijana Fermic
  */
-public class NavigacijaController {
+public class NavigacijaController implements Initializable {
+
+    @FXML
+    Button pocetnaBtn, oNamaBtn, kontaktBtn, logoutBtn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        pocetnaBtn.setCursor(Cursor.HAND);
+        oNamaBtn.setCursor(Cursor.HAND);
+        kontaktBtn.setCursor(Cursor.HAND);
+        logoutBtn.setCursor(Cursor.HAND);
+    }
 
     public void onPocetna(ActionEvent actionEvent) throws IOException {
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
